ce213b3f9b634d43bcdbc8b896636ef4cd35c2f8
MiguelGil <miguelgilmartinez@gmail.com>
2021-07-16 12:22:30 +0200

    Added DateTime GraphQL scalar

---
+++ src/GraphQL/Resolver/HealthDataResolver.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Maps GraphQL petitions to Symfony
+ *
+ * @author miguelgilmartinez@gmail.com
+ */
+
+namespace App\GraphQL\Resolver;
+
+use App\Entity\HealthData;
+use Doctrine\ORM\EntityManagerInterface;
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+
+class HealthDataResolver implements ResolverInterface, AliasedInterface
+{
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function findAllHealthData(Argument $args)
+    {
+        $values = $this->em->getRepository(HealthData::class)->findAll();
+        return $values;
+    }
+
+    private function convertDateTimeToString(?\DateTime $date)
+    {
+        return is_null($date) ? '': $date->format('Y-m-d H:i:s');
+    }
+
+    // public function findByID(Argument $args) {
+    //     return $this->em->getRepository(HealthData::class)
+    //                     ->findOneById($args['userUUID']);
+    // }
+
+    public static function getAliases(): array
+    {
+        return [
+            'findAllHealthData' => 'all_healthdata'
+            // 'findByID' => 'user_by_id'
+        ];
+    }
+}
